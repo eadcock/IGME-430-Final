ee400dc9c65a64d450389bde0fbbabfc58664932
Eric Adcock <eta4411@rit.edu>
2021-05-08 13:11:21 -0400

    init w/ domomaker

---
+++ server/controllers/Account.js
@@ -0,0 +1,86 @@
+const models = require('../models');
+
+const { Account } = models;
+
+const loginPage = (req, res) => {
+  res.render('login', { csrfToken: req.csrfToken() });
+};
+
+const signupPage = (req, res) => {
+  res.render('signup', { csrfToken: req.csrfToken() });
+};
+
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
+
+const login = (req, res) => {
+  const username = `${req.body.username}`;
+  const password = `${req.body.pass}`;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'RAWR! All fields are required' });
+  }
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username or password' });
+    }
+
+    req.session.account = Account.AccountModel.toAPI(account);
+
+    return res.json({ redirect: '/maker' });
+  });
+};
+
+const signup = (req, res) => {
+  req.body.username = `${req.body.username}`;
+  req.body.pass = `${req.body.pass}`;
+  req.body.pass2 = `${req.body.pass2}`;
+
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! All fields are required' });
+  }
+  if (req.body.pass !== req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! Passwords do not match' });
+  }
+
+  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+    const accountData = {
+      username: req.body.username,
+      salt,
+      password: hash,
+    };
+
+    const newAccount = new Account.AccountModel(accountData);
+
+    newAccount.save().then(() => {
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      res.json({ redirect: '/maker' });
+    }).catch((err) => {
+      console.log(err);
+
+      if (err.code === 11000) {
+        return res.status(400).json({ error: 'Username already in use.' });
+      }
+
+      return res.status(400).json({ error: 'An error ocurred. ' });
+    });
+  });
+};
+
+const getToken = (req, res) => {
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
+};
+
+module.exports.loginPage = loginPage;
+module.exports.login = login;
+module.exports.logout = logout;
+module.exports.signupPage = signupPage;
+module.exports.signup = signup;
+module.exports.getToken = getToken;
